528181f4b117e2f48ffe862392a7eb1bc36ae6df
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2025-07-16 17:17:21 -0600

    Buttons grid

--- src/components/Carcasa.jsx
+++ src/components/Carcasa.jsx
@@ -9,22 +9,24 @@ function Carcasa() {
     <div className="carcasa">
       <h1>Calculadoras<br/>LRS</h1>
       <Display></Display>
-      <Operador id="add"></Operador>
-      <Operador id="subtract"></Operador>
-      <Operador id="multiply"></Operador>
-      <Operador id="divide"></Operador>
-      <Numero id="one">1</Numero>
-      <Numero id="two">2</Numero>
-      <Numero id="three">3</Numero>
-      <Numero id="four">4</Numero>
-      <Numero id="five">5</Numero>
-      <Numero id="six">6</Numero>
-      <Numero id="seven">7</Numero>
-      <Numero id="eight">8</Numero>
-      <Numero id="nine">9</Numero>
-      <Numero id="zero">0</Numero>
-      <Numero id="decimal">.</Numero>
-      <Limpiar></Limpiar>
+      <div className="grid-botones">
+        <Operador id="add"></Operador>
+        <Operador id="subtract"></Operador>
+        <Operador id="multiply"></Operador>
+        <Operador id="divide"></Operador>
+        <Numero id="one">1</Numero>
+        <Numero id="two">2</Numero>
+        <Numero id="three">3</Numero>
+        <Numero id="four">4</Numero>
+        <Numero id="five">5</Numero>
+        <Numero id="six">6</Numero>
+        <Numero id="seven">7</Numero>
+        <Numero id="eight">8</Numero>
+        <Numero id="nine">9</Numero>
+        <Numero id="zero">0</Numero>
+        <Numero id="decimal">.</Numero>
+        <Limpiar></Limpiar>
+      </div>
     </div>
   )
 }
