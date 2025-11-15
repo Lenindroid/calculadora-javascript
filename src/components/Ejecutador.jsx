e4fc7262f13fc64959ddc6f6c75d7cfab237a4ae
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2025-11-14 19:08:06 -0600

    Regex conditions

--- src/components/Ejecutador.jsx
+++ src/components/Ejecutador.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
-function Ejecutador({children, currentOperation}) {
+function Ejecutador({children, currentOperation, setOperation}) {
   function exe() {
-    if(currentOperation[currentOperation.length - 1] == '÷' 
-      || currentOperation[currentOperation.length - 1] == '+'
-      || currentOperation[currentOperation.length - 1] == '-'
-      || currentOperation[currentOperation.length - 1] == 'x'
-      || currentOperation[currentOperation.length - 1] == '.') currentOperation.slice(0, -1);
+    const digitosDeInicio = /^\d(?:.*\d)?$/;
+    const charAllowed = /^[\d÷+\-x.]+$/;
+    const consecutiveOps = /[÷+\-x.]{2,}/;
+    if (digitosDeInicio.test(currentOperation) && charAllowed.test(currentOperation) && !consecutiveOps.test(currentOperation)) {
+      console.log("String válido.");
+    } else {
+      console.log("String no válido.");
+      if (!digitosDeInicio.test(currentOperation)) console.log("Un digito debe estar al inicio al final del string");
+      if (!charAllowed.test(currentOperation)) console.log("Hay caracteres invalidos");
+      if (consecutiveOps.test(currentOperation)) console.log("No pueden haber simbolos especiales consecutivos.");
+      setOperation("ERROR");
+    }
   }
 
   return (
